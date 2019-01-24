9293740bf68eb132382a6dbc23a38a3d32b3603a
Administrator <Administrator@192.168.1.101>
2019-01-24 22:30:02 +0800

    标题:添加剩余的切入点简例  时间:2019年1月24日22:28:37  内容:添加剩余的After和Around切入点简例

--- springDemo/src/main/java/com/spring/aop/MyAspect.java
+++ springDemo/src/main/java/com/spring/aop/MyAspect.java
@@ -1,6 +1,7 @@
 package com.spring.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 
 public class MyAspect {
 
@@ -10,5 +11,17 @@ public class MyAspect {
 	
 	public void myAfterThrowing(JoinPoint joinPoint,Exception exception) {
 		System.out.println("myAfterThrowing执行了,目标方法抛出的异常是:"+exception);
+	}
+	
+	public void myAfter(JoinPoint joinPoint) {
+		System.out.println("myAfter执行了");
+	}
+	
+	public Object myAround(ProceedingJoinPoint joinPoint)throws Throwable {
+		//很类似前面jdk代理中InvocationHandler的invoke
+		System.out.println("myAround的预处理");
+		Object returnValue=joinPoint.proceed();
+		System.out.println("myAround的后处理");
+		return returnValue;
 	}
 }
--- springDemo/src/main/java/com/spring/aop/Test1.java
+++ springDemo/src/main/java/com/spring/aop/Test1.java
@@ -15,18 +15,32 @@ public class Test1 {
 		proxyObject.show("hi");
 	}
 	
-	@Test
+	@Test//myAfterReturnning
 	public void test2() {
 		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("com/spring/aop/beans.xml");
 		MyInterface proxyObject=applicationContext.getBean(MyInterface.class);//如果使用接口实现方式,则需要使用接口对象代理调用方法
 		proxyObject.show("hi");
 	}
 	
-	@Test
+	@Test//myAfterThrowing
 	public void test3() throws Exception {
 		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("com/spring/aop/beans.xml");
 		MyInterface proxyObject=applicationContext.getBean(MyInterface.class);//如果使用接口实现方式,则需要使用接口对象代理调用方法
 		proxyObject.show(16);
+	}
+	
+	@Test//myAfter,在最后面执行,如此处在myAfterReturnning之后才执行myAfter
+	public void test4() {
+		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("com/spring/aop/beans.xml");
+		MyInterface proxyObject=applicationContext.getBean(MyInterface.class);//如果使用接口实现方式,则需要使用接口对象代理调用方法
+		proxyObject.show("hi");
+	}
+	
+	@Test//myAround,实现了多个效果,处理效果在最先和最后位置
+	public void test5() {
+		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("com/spring/aop/beans.xml");
+		MyInterface proxyObject=applicationContext.getBean(MyInterface.class);//如果使用接口实现方式,则需要使用接口对象代理调用方法
+		proxyObject.show("hi");
 	}
 	
 }
